7d66eb5aac969f480a6271e4fc00b3ff43b825f3
RudrakshPaliwal <75530058+edithkaren@users.noreply.github.com>
2022-05-04 17:07:14 +0530

    Update ViewController.swift

--- Food Ordering App/Food Ordering App/ViewController.swift
+++ Food Ordering App/Food Ordering App/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
  
     var Billamt: Double = 0
@@ -17,43 +16,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     @IBOutlet weak var pizza: UISwitch{
     cost = 450.00
     }
     @IBAction func pizza(_ sender: UISwitch) {
         
         Billamt = cost + 120
-        billDetails = billDetails + "\n "+"Pizza!"
-        
+        billDetails = billDetails + "\n "+"Pizza!"   
     }
-    
-    
     weak var Coke: UISwitch!{
         cost = 50.00
     }
-    
     @IBAction func Coke(_ sender: Any) {
         Billamt = cost + 20
         billDetails = billDetails + "\n" + " Coke "
     }
-    
-
     weak var Burger: UISwitch!{
         cost = 150
     }
-    
     @IBAction func Burger(_ sender: UISwitch) {
         Billamt = cost + 40
         billDetails = billDetails + " " + "Burger"
     }
-    
-    
+
     @IBAction func PlaceButton(_ sender: UIButton) {
         }
-        
-        
-        
     }
 }
 
